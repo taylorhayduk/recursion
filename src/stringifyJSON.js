bed9d0a87f68b679469fde199204ca4073556a31
Taylor Hayduk <taylorhayduk@gmail.com>
2015-08-05 18:51:01 -0400

    Works thru arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,40 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // Should be same as: return JSON.stringify(obj);
+  console.log("-------------------------------------------------------");
+  console.log("The obj is: "+ obj);
+  console.log("The obj is of type: "+ typeof(obj))
+  console.log("The Stringified Actual is: "+JSON.stringify(obj));
+  
+  if (typeof(obj) === 'object') {
+  	if (obj === null) {
+  		return 'null';
+  	}
+  	if (Array.isArray(obj)) {
+  		var result = [];
+  		for (var i = 0; i < obj.length; i++) {
+  			result[i] = stringifyJSON(obj[i]);
+  		}
+  		console.log("["+result.toString()+"]");
+  		return ("["+result.toString()+"]");
+  	}
+  	var returnStr = "";
+  	for (key in obj) {
+  		returnStr.concat(key);
+  	}
+  	console.log("I'm in the obj loop!!!");
+  	console.log(returnStr);
+  	return '[]';
+  }
+  if (typeof(obj) === 'string') {
+  	var emptyStr = "\"";
+  	emptyStr = emptyStr.concat(obj);
+	emptyStr = emptyStr.concat("\"");
+	return emptyStr;
+  }
+  else {
+  	console.log("It was not an object.  It returned: "+obj.toString());
+  	return obj.toString();
+  }
 };
