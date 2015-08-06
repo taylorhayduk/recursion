ceef61314b8b01b1669aa4e66a06b7ecc6796411
Taylor Hayduk <taylorhayduk@gmail.com>
2015-08-05 20:27:57 -0400

    COMPLETE: stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,11 +6,12 @@
 var stringifyJSON = function(obj) {
   // your code goes here
   // Should be same as: return JSON.stringify(obj);
-  console.log("-------------------------------------------------------");
+/*  console.log("-------------------------------------------------------");
   console.log("The obj is: "+ obj);
   console.log("The obj is of type: "+ typeof(obj))
-  console.log("The Stringified Actual is: "+JSON.stringify(obj));
-  
+  console.log("The Stringified Actual is: "+JSON.stringify(obj));*/
+  console.log(obj);
+
   if (typeof(obj) === 'object') {
   	if (obj === null) {
   		return 'null';
@@ -20,25 +21,42 @@ var stringifyJSON = function(obj) {
   		for (var i = 0; i < obj.length; i++) {
   			result[i] = stringifyJSON(obj[i]);
   		}
-  		console.log("["+result.toString()+"]");
   		return ("["+result.toString()+"]");
   	}
-  	var returnStr = "";
-  	for (key in obj) {
-  		returnStr.concat(key);
+  	else {
+  		var objResult = "";
+  		for (var key in obj) {
+  			if ((typeof(obj[key]) !== "function") && (key !== "undefined")) {
+  				console.log("the key is: " +key);
+	  			var newKey = stringifyJSON(key);
+	  			var newVal = stringifyJSON(obj[key]);
+	  			objResult = objResult.concat(newKey+":"+newVal+",");
+  			}
+  		}
+  		if (objResult.length === 0) {
+  			return '{}';
+  		}
+  		objResult = objResult.substring(0, objResult.length-1);
+  		return ("{"+objResult+"}");
   	}
-  	console.log("I'm in the obj loop!!!");
-  	console.log(returnStr);
-  	return '[]';
   }
+
   if (typeof(obj) === 'string') {
   	var emptyStr = "\"";
   	emptyStr = emptyStr.concat(obj);
 	emptyStr = emptyStr.concat("\"");
 	return emptyStr;
   }
+
+  if (typeof(obj) === 'function') {
+  	return;
+  }
+
+
   else {
-  	console.log("It was not an object.  It returned: "+obj.toString());
-  	return obj.toString();
+  	if (obj === undefined) {
+  		return "";
+  	}
+  	return (obj.toString());
   }
 };
