c0cac519f4ca0ac6ebd3c538673e915e4fe5bf10
Taylor Hayduk <taylorhayduk@gmail.com>
2015-08-06 00:17:54 -0400

    Use .push for readability, added comments

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,20 +7,26 @@
 var getElementsByClassName = function(className){
   var body = document.body;
   var result = [];
+
+  // recursive function goes thru all parent's decendants
+  // and appends ones with className to result
   var getChildClasses = function(parent, className) {
+  	// push current element if className exists
   	if (parent.classList.contains(className)) {
-  		result[result.length] = parent;
+  		result.push(parent);
   	}
+  	// recursive call on all children of current (parent) element
   	if (parent.hasChildNodes()) {
   		var children = parent.childNodes;
   		_.each(children, function(child){
+  			// skip #text nodes
   			if (child.nodeType !== 3) {
   			  getChildClasses(child, className);
   			}
   		});
   	  }
     };
-
+  // call the function
   getChildClasses(body, className);
   return result;
 };
